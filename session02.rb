8eefd4df18449fa6e3c1f6fe4b91ebb10f208459
Diego Mota Arriaga <diego.mota@wizeline.com>
2022-05-20 13:56:50 -0500

    feature: Session02

---
+++ session02.rb
@@ -0,0 +1,52 @@
+class Session02
+  def initialize
+  end
+
+  # test boolean in ruby
+  def test_boolean
+    puts 'true && false = ' + (true && false).to_s
+    puts 'true || false = ' + (true || false).to_s
+    puts '!true = ' + (!true).to_s
+    puts '!false = ' + (!false).to_s
+  end
+
+  # test regular expressions in ruby
+  def test_regexp
+    puts '"abc" =~ /abc/ = ' + ('abc' =~ /abc/).to_s
+    puts '"abc" =~ /bcd/ = ' + ('abc' =~ /bcd/).to_s
+    puts '"abc" =~ /^a/ = ' + ('abc' =~ /^a/).to_s
+    puts '"abc" =~ /c$/ = ' + ('abc' =~ /c$/).to_s
+    puts '"abc" =~ /^a|b/ = ' + ('abc' =~ /^a|b/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+    puts '"abc" =~ /^a|b|c$/ = ' + ('abc' =~ /^a|b|c$/).to_s
+  end
+
+  # test number bases in ruby
+  def test_number_base
+    puts '0xFF = ' + (0xFF).to_s
+    puts '0b11111111 = ' + (0b11111111).to_s
+    puts '0o377 = ' + (0o377).to_s
+    puts '0d377 = ' + (0d377).to_s
+    puts '0377 = ' + (0377).to_s
+  end
+
+  # test ranges in ruby
+  def test_range
+    puts '1..10 = ' + (1..10).to_s
+    puts '1...10 = ' + (1...10).to_s
+  end
+
+end
+
+session = Session02.new
+session.test_boolean
+session.test_regexp
+session.test_number_base
+session.test_range
